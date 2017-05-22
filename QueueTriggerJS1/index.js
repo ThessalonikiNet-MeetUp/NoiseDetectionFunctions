db5683e994e314960e030bfae661f4d2ab966d97
Rita Zhang <rita.z.zhang@gmail.com>
2017-05-22 05:03:31 -0700

    Add azure table support

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,24 +1,23 @@
-//var azure = require('azure-storage');
+var azure = require('azure-storage');
 
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
 
     // Lookup data
-    // var tableSvc = azure.createTableService();
+    var tableSvc = azure.createTableService();
 
-    // var query = new azure.TableQuery().top(5).where('PartitionKey eq ?', myQueueItem.deviceID);
+    var query = new azure.TableQuery().top(5).where('PartitionKey eq ?', myQueueItem.deviceID);
 
-    // tableSvc.queryEntities('ndbnotifications', query, null, function(error, result, response){
-    //     if(!error){
-    //         context.log('Retrieved ', result);
-    //     }
-    //     else {
-    //         context.log('error', err);
-    //     }
+    tableSvc.queryEntities('ndbnotifications', query, null, function(error, result, response){
+        if(!error){
+            context.log('Retrieved ', result);
+        }
+        else {
+            context.log('error', err);
+        }
         
-    //     context.done();
-    // });   
-    context.done();
+        context.done();
+    });  
     // Trigger the bot
 
 };
