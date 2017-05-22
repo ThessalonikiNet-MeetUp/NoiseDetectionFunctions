57c79abe2857c788775f41f039daede9c426a578
tkaragiannis <tasos.karagiannis@intrasoft-intl.com>
2017-05-22 17:04:06 +0300

    add sequelize and env

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,23 +1,61 @@
-var azure = require('azure-storage');
+const Sequelize = require('sequelize');
+
+const db = process.env.DB;
+const dbuser = process.env.DBUSER;
+const dbpwd = process.env.DBPWD;
+const dbhost = process.env.DBHOST;
+
+const sequelize = new Sequelize(db, dbuser, dbpwd, {
+  host: dbhost,
+  dialect: 'mssql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  dialectOptions: {
+    encrypt: true
+  }
+});
+
+const User = sequelize.define('users', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  email: Sequelize.STRING,
+  token: Sequelize.STRING,
+  channelid: Sequelize.STRING,
+  botuserid: Sequelize.STRING,
+  botusername: Sequelize.STRING,
+  deviceid: Sequelize.INTEGER
+});
+
+const Device = sequelize.define('devices', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  name: Sequelize.STRING
+});
 
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
 
-    // Lookup data
-    var tableSvc = azure.createTableService();
+    // sequelize.sync()
+    // .then(() => User.create({
+    //     username: 'janedoe',
+    //     birthday: new Date(1980, 6, 20)
+    // }))
+    // .then(jane => {
+    //     console.log(jane.get({
+    //     plain: true
+    //     }));
+    // });
 
-    var query = new azure.TableQuery().top(5).where('PartitionKey eq ?', myQueueItem.deviceID);
-
-    tableSvc.queryEntities('ndbnotifications', query, null, function(error, result, response){
-        if(!error){
-            context.log('Retrieved ', result);
+    var user = User.findAll({
+        where: {
+            email: 'ritazh@microsoft.com'
         }
-        else {
-            context.log('error', err);
-        }
-        
-        context.done();
-    });  
-    // Trigger the bot
-
+    }).then(function(response) {
+      context.log('result', response);
+      context.done();
+    }, function(error) {
+      context.log('error', error);
+      context.done();
+    });
 };
