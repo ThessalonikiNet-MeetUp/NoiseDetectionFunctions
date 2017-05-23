345256a1f9c759d83bc30ef8a9173a65e6791ede
Rita Zhang <rita.z.zhang@gmail.com>
2017-05-23 07:08:01 -0700

    Add directline communication

--- SendNotificationQueueTrigger/index.js
+++ SendNotificationQueueTrigger/index.js
@@ -1,17 +1,34 @@
 const db = require('./db');
-const DirectLine = require('./direct-line');
+const Sequelize = require('sequelize');
+const rp = require('request-promise');
+const Swagger = require('swagger-client');
+
+const directLineSecret = process.env.DIRECT_LINE_SECRET;
+const directLineClientName = 'DirectLineClient';
+const directLineSpecUrl = 'https://docs.botframework.com/en-us/restapi/directline3/swagger.json';
 
 const User = db.User;
 const Device = db.Device;
 
+var directLineClient = rp(directLineSpecUrl)
+  .then(function (spec) {
+    // client
+    return new Swagger({
+        spec: JSON.parse(spec.trim()),
+        usePromise: true,
+        authorizations : {
+        AuthorizationBotConnector: new Swagger.ApiKeyAuthorization('Authorization', `Bearer ${directLineSecret}`, 'header'),
+      }
+    });
+})
+.catch(function (err) {
+    context.log('Error initializing DirectLine client', err);
+});
+
 module.exports = function (context, myQueueItem) {
 
   context.log('JavaScript queue trigger function processed work item', myQueueItem);
-
-  var directLine = new DirectLine(process.env.DIRECT_LINE_SECRET);
-
   var deviceID = myQueueItem.deviceID;
-
   Device.find({
     where: {
         id: deviceID
@@ -27,6 +44,36 @@ module.exports = function (context, myQueueItem) {
       }
 
       context.log('user id', response.dataValues.user.id);
+      context.log('set client')
+
+      directLineClient.then(function (client) {
+        context.log('start convo');
+        client.Conversations.Conversations_StartConversation()                          // create conversation
+          .then(function (response) {
+              context.log(response.obj.conversationId);
+              return response.obj.conversationId;
+          })                            // obtain id
+          .then(function (conversationId) {
+             context.log(conversationId);
+             context.log('post');
+
+             client.Conversations.Conversations_PostActivity(
+             {
+                  conversationId: conversationId,
+                  activity: {
+                      textFormat: 'plain',
+                      text: "{'userid', '" + response.dataValues.user.id+ "'}",
+                      type: 'message',
+                      from: {
+                          id: directLineClientName,
+                          name: directLineClientName
+                      }
+                  }
+              }).catch(function (err) {
+                  context.log('Error sending message:', err);
+              });
+          });
+      });
       context.done();
       
     }, function(error) {
