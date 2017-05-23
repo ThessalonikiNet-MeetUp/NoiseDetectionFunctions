043c29b40f59cccb754526155381e4eaf0ecbade
tkaragiannis <tasos.karagiannis@intrasoft-intl.com>
2017-05-23 10:37:31 +0300

    Updated user table

--- SendNotificationQueueTrigger/index.js
+++ SendNotificationQueueTrigger/index.js
@@ -20,12 +20,14 @@ const sequelize = new Sequelize(db, dbuser, dbpwd, {
 
 const User = sequelize.define('users', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  email: Sequelize.STRING,
-  token: Sequelize.STRING,
-  channelid: Sequelize.STRING,
-  botuserid: Sequelize.STRING,
-  botusername: Sequelize.STRING,
-  deviceid: Sequelize.INTEGER
+  email: { type: Sequelize.STRING },
+  name: { type: Sequelize.STRING },
+  botid: { type: Sequelize.STRING },
+  botname: { type: Sequelize.STRING },
+  serviceurl: { type: Sequelize.STRING },
+  token: { type: Sequelize.STRING },
+  conversationid: { type: Sequelize.STRING },
+  channelid: { type: Sequelize.STRING }
 },
 {
   timestamps: false
@@ -33,18 +35,24 @@ const User = sequelize.define('users', {
 
 const Device = sequelize.define('devices', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  name: Sequelize.STRING
+  name: { type: Sequelize.STRING },
+  userid: { type: Sequelize.INTEGER }
+},
+{
+  timestamps: false
 });
 
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
 
-    var user = User.findAll({
+    User.findAll({
         where: {
             email: 'ritazh@microsoft.com'
         }
     }).then(function(response) {
-      context.log('result', response);
+      var user = response[0].dataValues;
+
+      context.log('result', user);
       context.done();
     }, function(error) {
       context.log('error', error);
