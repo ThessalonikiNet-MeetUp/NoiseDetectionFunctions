a54612858ca57af17b604b10ebd8bd8fde0068c2
ktasos <k_tasos82@hotmail.com>
2017-05-23 12:46:20 +0300

    Shared file for models definition

--- SendNotificationQueueTrigger/index.js
+++ SendNotificationQueueTrigger/index.js
@@ -1,65 +1,35 @@
-const Sequelize = require('sequelize');
+const db = require('../db');
 const DirectLine = require('./direct-line');
 
-const db = process.env.DB;
-const dbuser = process.env.DBUSER;
-const dbpwd = process.env.DBPWD;
-const dbhost = process.env.DBHOST;
+const User = db.User;
+const Device = db.Device;
 
-const sequelize = new Sequelize(db, dbuser, dbpwd, {
-  host: dbhost,
-  dialect: 'mssql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  dialectOptions: {
-    encrypt: true
-  }
-});
-
-const User = sequelize.define('users', {
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  email: { type: Sequelize.STRING },
-  name: { type: Sequelize.STRING },
-  botid: { type: Sequelize.STRING },
-  botname: { type: Sequelize.STRING },
-  serviceurl: { type: Sequelize.STRING },
-  token: { type: Sequelize.STRING },
-  conversationid: { type: Sequelize.STRING },
-  channelid: { type: Sequelize.STRING },
-  userid: { type: Sequelize.STRING }
-},
-{
-  timestamps: false
-});
-
-const Device = sequelize.define('devices', {
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: Sequelize.STRING },
-  userid: { type: Sequelize.INTEGER }
-},
-{
-  timestamps: false
-});
+var errorHandler = function(err) {
+  context.log('error', error);
+  context.done();
+}
 
 module.exports = function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
+  context.log('JavaScript queue trigger function processed work item', myQueueItem);
 
-    var directLine = new DirectLine(process.env.DIRECT_LINE_SECRET);
+  var directLine = new DirectLine(process.env.DIRECT_LINE_SECRET);
 
-    User.findAll({
-        where: {
-            email: 'ritazh@microsoft.com'
-        }
-    }).then(function(response) {
-      var user = response[0].dataValues;
+  var deviceID = myQueueItem.deviceID;
 
-      context.log('result', user);
+  Device.find({
+    where: {
+        id: deviceID
+    }, include: [User]}).then(function(response) {
+      if(response === null) {
+        errorHandler({source: 'f', message: 'device not found'});
+      }
+      var device = response.dataValues;
+      if(device.user === null) {
+        errorHandler({source: 'f', message: 'user info could not be retrieve'});
+      }
+
+      context.log('user id', response.dataValues.user.id);
+
       context.done();
-    }, function(error) {
-      context.log('error', error);
-      context.done();
-    });
+    }, errorHandler);
 };
---
+++ db.js
@@ -0,0 +1,52 @@
+const Sequelize = require('sequelize');
+
+const db = process.env.DB;
+const dbuser = process.env.DBUSER;
+const dbpwd = process.env.DBPWD;
+const dbhost = process.env.DBHOST;
+
+const sequelize = new Sequelize(db, dbuser, dbpwd, {
+  host: dbhost,
+  dialect: 'mssql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  dialectOptions: {
+    encrypt: true
+  }
+});
+
+const User = sequelize.define('users', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: Sequelize.STRING },
+  name: { type: Sequelize.STRING },
+  botid: { type: Sequelize.STRING },
+  botname: { type: Sequelize.STRING },
+  serviceurl: { type: Sequelize.STRING },
+  token: { type: Sequelize.STRING },
+  conversationid: { type: Sequelize.STRING },
+  channelid: { type: Sequelize.STRING },
+  userid: { type: Sequelize.STRING }
+},
+{
+  timestamps: false
+});
+
+const Device = sequelize.define('devices', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: Sequelize.STRING },
+  userid: { type: Sequelize.INTEGER }
+},
+{
+  timestamps: false
+});
+
+User.hasMany(Device, {foreignKey: 'userid'});
+Device.belongsTo(User, {foreignKey: 'userid'});
+
+module.exports = {
+    User: User,
+    Device: Device
+}
