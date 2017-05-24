0e6e82ffa214139d0612d7adc5255ffa43b21555
Jason Poon <git@jasonpoon.ca>
2017-05-24 12:34:06 +0300

    update sequelize

--- SendNotificationQueueTrigger/db.js
+++ SendNotificationQueueTrigger/db.js
@@ -22,12 +22,19 @@ const User = sequelize.define('users', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: Sequelize.STRING },
   name: { type: Sequelize.STRING },
+  token: { type: Sequelize.STRING },
+},
+{
+  timestamps: false
+});
+
+const BotInfo = sequelize.define('botinfo', {
   botid: { type: Sequelize.STRING },
   botname: { type: Sequelize.STRING },
   serviceurl: { type: Sequelize.STRING },
-  token: { type: Sequelize.STRING },
   conversationid: { type: Sequelize.STRING },
   channelid: { type: Sequelize.STRING },
+  botuserid: { type: Sequelize.STRING },
   userid: { type: Sequelize.STRING }
 },
 {
@@ -45,6 +52,9 @@ const Device = sequelize.define('devices', {
 
 User.hasMany(Device, {foreignKey: 'userid'});
 Device.belongsTo(User, {foreignKey: 'userid'});
+
+User.hasMany(BotInfo, {foreignKey: 'userid'});
+BotInfo.belongsTo(User, {foreignKey: 'userid'});
 
 module.exports = {
     User: User,
